efcf0133985a597a1c85bc9e1924bccf7dad2a29
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-05-22 15:24:39 -0400

    CRU

---
+++ app/controllers/genres_controller.rb
@@ -0,0 +1,34 @@
+class GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def new
+    @genre = Genre.new
+  end
+
+  def create
+    @genre = Genre.create(require_params)
+    redirect_to genre_path(@genre)
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    @genre.update(require_params)
+    redirect_to genre_path(@genre)
+  end
+
+  private
+  def require_params
+    params.require(:genre).permit(:name)
+  end
+end
